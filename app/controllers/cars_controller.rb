04c6500ccfae4c7b7d03d054b25244006a48010f
Caio Lopes <caio.mlopes@icloud.com>
2026-05-28 23:01:21 -0400

    feat: frontend com tema claro/escuro e homepage dedicada

--- app/controllers/cars_controller.rb
+++
@@ -1,9 +0,0 @@
-class CarsController < ApplicationController
-  def index
-    @cars = Car.order(:brand, :name, :year)
-  end
-
-  def show
-    @car = Car.find(params[:id])
-  end
-end
---
+++ app/controllers/vehicles_controller.rb
@@ -0,0 +1,9 @@
+class VehiclesController < ApplicationController
+  def index
+    @vehicles = Vehicle.includes(:brand).order("brands.name", :name, :version, :year)
+  end
+
+  def show
+    @vehicle = Vehicle.includes(:brand, :technical_spec, :maintenances, :defects, :reviews, :fipe_prices).find(params[:id])
+  end
+end
